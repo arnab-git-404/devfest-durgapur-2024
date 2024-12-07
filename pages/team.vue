e5ca470b4fcd7ed5130a5b98ede46ee14e86be46
Arnab Mukherjee <136512754+arnab-git-404@users.noreply.github.com>
2024-12-07 21:07:54 +0530

    Update team.vue

--- pages/team.vue
+++ pages/team.vue
@@ -43,16 +43,6 @@
       </v-row>
           
       
-      <!-- Management Team -->
-      <v-row v-if="outreachTeam.length">
-        <v-col md="12">
-          <h2>Management Team</h2>
-        </v-col>
-        <v-col md="2" sm="3" cols="6" v-for="(item, index) in ManagementTeam" :key="index">
-          <common-team-card :data="item" />
-        </v-col>
-      </v-row>
-      
       
       <!-- Outreach Team -->
       <v-row v-if="outreachTeam.length">
